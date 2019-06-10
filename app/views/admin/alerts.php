651018a214132f9a95e0188e3ae55bf823ebae9a
ivanmitic91 <ivanmitic91@gmail.com>
2019-06-10 23:03:18 +0200

    Added php extension to files ,  transfered admin files to view/admin  , separate header, footer in inc/admin

---
+++ app/views/admin/alerts.php
@@ -0,0 +1,20 @@
+<?php
+require_once '../inc/admin/adminheader.php';
+require_once '../inc/admin/adminnav.php';
+?>
+
+<div class="breadcome-area">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+        Content
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+require_once '../inc/admin/adminfooter.php';
+?>
