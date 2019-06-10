d20838e707b4659cf8d366ddaed6663b7056b34b
Slobodan Lazić <slobodan.lazic.nbg@gmail.com>
2019-06-10 17:31:47 +0200

    added navbar user model users controller

--- app/models/User.php
+++ app/models/User.php
@@ -1,1 +1,12 @@
 <?php
+    class User extends Controller
+    {
+        private $db;
+
+        public function __construct()
+        {
+            $this->db = new Database();
+        }
+    }
+    
+?>
