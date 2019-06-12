8e91d289007fe8a5a13b2028c4f518d60034e0d6
ivanmitic91 <ivanmitic91@gmail.com>
2019-06-12 22:18:23 +0200

    Added insert student form , working insert student expect loosing select data after redirect

--- app/controllers/Students.php
+++ app/controllers/Students.php
@@ -26,8 +26,67 @@ class Students extends Controller
 
   public function insert()
   {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      // Sanitize POST
+      $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
-    $this->view('admin/students/insert');
+      $data = [
+        'first_name' => trim($_POST['first_name']),
+        'last_name' => trim($_POST['last_name']),
+        'id_school_class' => trim($_POST['id_school_class']),
+
+        'first_name_err' => '',
+        'last_name_err' => '',
+        'id_school_class_err' => ''
+      ];
+
+      // Validate first name
+      if (empty($data['first_name'])) {
+        $data['first_name_err'] = 'Please enter first name';
+      }
+
+      // Validate last name
+      if (empty($data['last_name'])) {
+        $data['last_name_err'] = 'Please enter last name';
+      }
+
+      // Validate class 
+
+      if (empty($data['id_school_class'])) {
+        $data['id_school_class_err'] = 'Please select class';
+      }
+
+      // Make sure there are no errors
+      if (empty($data['first_name_err']) && empty($data['last_name_err']) && empty($data['id_school_class_err'])) {
+        // Validation passed
+        //Execute
+        if ($this->studentModel->insertStudent($data)) {
+          // Redirect to login
+          // flash('post_added', 'Student Added');
+          redirect('admin/students/index');
+        } else {
+          die('Something went wrong');
+        }
+      } else {
+        // Load view with errors
+        $this->view('admin/students/insert', $data);
+      }
+    } else {
+      $data = [
+        'first_name' => '',
+        'last_name' => '',
+        'id_school_class' => '',
+        'first_name_err' => '',
+        'last_name_err' => '',
+        'id_school_class_err' => '',
+      ];
+
+      $classes = $this->studentModel->showAllClasses();
+
+      $data['classes'] = $classes;
+
+      $this->view('admin/students/insert', $data);
+    }
   }
 
   public function edit()
