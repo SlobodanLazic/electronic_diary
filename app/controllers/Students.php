909ba6ad358a8b5eb99a76fc33a6a7224bedde4d
ivanmitic91 <ivanmitic91@gmail.com>
2019-06-12 15:11:48 +0200

    Working Admin dashboart student links and method for selecting all students

--- app/controllers/Students.php
+++ app/controllers/Students.php
@@ -1,23 +1,44 @@
 <?php
 
- Class Students extends Controller {
+class Students extends Controller
+{
 
-    public function __construct()
-    {
-        $this->studentModel = $this->model('Student');
-    }
-
-
-
-public function about(){
-
-      $data = [
-        'title' => 'About Us'
-      ];
-
-      $this->view('pages/about', $data);
-    }
+  public function __construct()
+  {
+    $this->studentModel = $this->model('Student');
+  }
 
 
 
+  public function index()
+  {
+
+    $students = $this->studentModel->showAllStudents();
+
+    $data = [
+
+      'students' => $students
+
+    ];
+
+    $this->view('admin/students/index', $data);
+  }
+
+  public function insert()
+  {
+
+    $this->view('admin/students/insert');
+  }
+
+  public function edit()
+  {
+
+    $this->view('admin/students/edit');
+  }
+
+  public function delete()
+  {
+
+    $this->view('admin/students/delete');
+  }
 }
--- app/models/Student.php
+++ app/models/Student.php
@@ -1,59 +1,53 @@
 <?php
-    class Student  
+class Student
+{
+    private $db;
+
+    public function __construct()
     {
-        private $db;
-
-        public function __construct()
-        {
-            $this->db = new Database();
-        }
-
-
-        public function insertStudent(){
-
-            $this->db->query('INSERT INTO students (first_name , last_name , id_class) VALUES (:first_name , :last_name , :id_class)');
-
-        }
-
-        public function editStudent(){
-
-            $this->db->query('UPDATE students SET first_name = :first_name , last_name = :last_name , id_class = :id_class');
-            
-        }
-
-
-        public function deleteStudent(){
-
-            $this->db->query('DELETE FROM students WHERE id_student = :id_student');
-            
-        }
-
-        
-        public  function showAllStudents(){
-
-            $this->db->query('SELECT * FROM students');
-
-            $users = $this->db->resultSet();
-
-            return $users;
-
-        
-        }
-
-        public function getStudentById($id_student){
-
-            $this->db->query('SELECT * FROM students WHERE id_student = :id_student');
-
-            $this->db->bind(':id_student', $id);
-
-            $row = $this->db->single();
-
-            return $row;
-
-        }
-
+        $this->db = new Database();
     }
 
-    
-    
-?>
+
+    public function insertStudent()
+    {
+
+        $this->db->query('INSERT INTO students (first_name , last_name , id_class) VALUES (:first_name , :last_name , :id_class)');
+    }
+
+    public function editStudent()
+    {
+
+        $this->db->query('UPDATE students SET first_name = :first_name , last_name = :last_name , id_class = :id_class');
+    }
+
+
+    public function deleteStudent()
+    {
+
+        $this->db->query('DELETE FROM students WHERE id_student = :id_student');
+    }
+
+
+    public  function showAllStudents()
+    {
+
+        $this->db->query('SELECT * FROM students');
+
+        $students = $this->db->resultSet();
+
+        return $students;
+    }
+
+    public function getStudentById($id_student)
+    {
+
+        $this->db->query('SELECT * FROM students WHERE id_student = :id_student');
+
+        $this->db->bind(':id_student', $id);
+
+        $row = $this->db->single();
+
+        return $row;
+    }
+}
