9f6f7ac0522857c06a56ca536d3ff3a8ececde58
Vaskrsije <panicvaskrsije@gmail.com>
2019-06-19 10:56:29 +0200

    edit subjects

--- app/controllers/Subjects.php
+++ app/controllers/Subjects.php
@@ -7,22 +7,89 @@ class Students extends Controller
         $this->subjectModel = $this->model('Subject');
     }
     public function index(){
-        
+        $subjects = $this->subjectModel->showallSubjects();
+
+        $data = [
+            'subjects' => $subjects
+        ];
+
+        $this->view('admin/subjects/index', $data);
     }
     public function insert(){
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        // Sanitise post
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        $data = [
+            'name' => trim($_POST['name']),
 
+            'name_err' => ''
+    ];
+
+    // Validate name
+    if(empty($data['name'])){
+        $data['name'] = 'Please enter subject name';
+    }
+
+    if(empty($data['name'])) {
+        if($this->subjectModel->insertSubject($data)) {
+            flash('subject_message', 'Subject added');
+            redirect('/subjects');
+        } else {
+            die('Something went wrong');
+        }
+    }
+
+        }
     }
     public function edit($id){
-    
+        $subject = $this->subjectModel->getSubjectId($id);
+
+        $data = [
+            'subject' => $subject
+        ];
+
+        $this->view('/admin/subjects/edit', $data);
     }
     public function delete($id){
-
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if($this->subjectModel->deleteSubject($id)) {
+                flash('subject_deleted_msg', 'Subject Deleted');
+                redirect('subjects');
+            } else {
+                die('Something went wrong');
+            }
+        } else {
+            redirect('pages');
+        }
     }
     public function show($id){
-
+        $subject = $this->subjectModel()->getSubjectId($id);
+        
+        $data = [
+            'subject' => $subject
+        ];
+        $this->view();
     }
     public function update(){
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
+            $data = [
+                'name' => trim($_POST['name']),
+
+                'name_err' => ''
+            ];
+
+        // Validate for name
+        if(empty($data['name'])) {
+            $data['name_err'] = 'Please enter a subject';
+        }
+
+        if(empty($data['name_err'])) {
+            if($this->subjectModel->update($data))
+        }
+
+        }
     }
 
 }
--- app/models/Subject.php
+++ app/models/Subject.php
@@ -9,7 +9,7 @@ class Student
     }
 
 
-    public function insertSubject($data)
+    public function insert($data)
     {
         $this->db->query('INSERT INTO subjects (name) VALUES (:name)');
 
@@ -22,13 +22,30 @@ class Student
         }
     }
 
-    public function editSubject()
+    public function showallSubjects() {
+        $this->db->query('SELECT name FROM subjects');
+
+        $subjects = $this->db->resultSet();
+
+        return $subjects;
+    }
+    public function edit()
     {
         $this->db->query('UPDATE subjects SET name = :name');
     }
+    public function getSubjectId($id)
+    {
+        $this->db->query('SELECT * FROM subjects WHERE id_subject = :id_subject');
+
+        $this->db->bind(':id_subject', $id);
+
+        $row = $this->db->single();
+
+        return $row;
+    }
 
 
-    public function deleteSubject($id)
+    public function delete($id)
     {
         $this->db->query('DELETE FROM subjects WHERE id_subject = :id_subject');
         $this->db->ind(':id_subject', $id);
@@ -43,6 +60,15 @@ class Student
 
     public function update($data)
     {
-        
+        $this->db->query('UPDATE subjects name = :name WHERE id_subject = :id');
+
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':id', $data['id_subject']);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
