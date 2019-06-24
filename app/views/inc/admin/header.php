3b13ebf24ef0d6e071642ab03963c52b28fce014
ivanmitic91 <>
2019-06-24 10:24:40 +0200

    Renamed index .php to insert.php for schedules insert

--- app/views/inc/admin/header.php
+++ app/views/inc/admin/header.php
@@ -212,7 +212,7 @@
                         <a href="javascript:;" data-toggle="collapse" data-target="#demo4"><i class="fa fa-fw fa-arrows-v"></i>Schedules <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="demo4" class="collapse">
                             <li>
-                                <a href="<?php echo URLROOT . '/schedules'; ?>">Insert Schedule</a>
+                                <a href="<?php echo URLROOT . '/schedules/insert'; ?>">Insert Schedule</a>
                             </li>
                             <li>
                                 <a href="#">Edit Schedule</a>
