d4a22f513fa581a13dcf77b2a26fa32e9f2bc53b
SlobodanLazic <slobodan.lazic.nbg@gmail.com>
2019-06-28 10:51:13 +0200

    Update user stylized buttons and updae pages

--- app/views/users/update.php
+++ app/views/users/update.php
@@ -13,69 +13,8 @@
                 <?php flash('user_message') ?>
                 <?php flash('user_updated_msg') ?>
 
-                <form action="<?php echo URLROOT; ?>/users/update" method="post">
-
-                    <div class="form-group">
-                        <label>Select class:</label>
-                        <select name="user_role" id="user_role" class="form-control form-control-lg <?php echo (!empty($data['user_role_err'])) ? 'is-invalid' : '' ?>">
-                                <option value='' selected>.....Select a role.....</option>
-                                <?php
-                                $users = new Users();
-                                foreach ($users->GetUserRoles() as $id_user_role => $name) :
-                                    ?>
-                                    <option value='<?php echo $id_user_role; ?>'><?php print($name); ?></option>
-                                <?php endforeach; ?>    
-                        </select>
-
-                        <input type="submit" class="btn btn-success" value="Show">  
-                    </div>
-
-                </form> 
-
-                <div class="table-responsive">
-                    <table class="table table-striped">
-
-                        <thead>
-
-                            <tr>
-                                <th>Username</th>
-                                <th>Email</th>
-                                <th>User Role</th>
-                                <th>Update</th>
-                            </tr>
-                        </thead>
-                        
-                        <?php 
-                        $users = new Users();
-                        // if isset post user role get that value if not get ''
-                        foreach ($users->GetUsersByUserRole($_POST["user_role"] ?? '') as $user) : ?>
-
-                            <tbody>
-
-                                <tr>
-                                    <td><?php echo($user->username); ?></td>
-                                    <td><?php print($user->email); ?></td>
-                                    <td><?php printf($user->name); ?></td>
-                                    <td>
-                                        <form action="<?php echo URLROOT . '/users/update/'; ?>" method="POST">
-                                            <input class="hidden" name="id_user" type="text" value="<?php echo $user->id_user; ?>">
-                                            <input type="submit" value="Update" class="btn btn-dark btn-block">
-                                        </form>
-                                    </td>
-                                </tr>
-
-                            </tbody>
-
-                        <?php endforeach; ?>
-
-                    </table>
-                </div>
-            </div>
-        </div>
-        <!-- /.row -->
-
         <!-- Page Heading -->
-        <div class="hidden row">
+        <div class="row">
             <div class="col-md-6 mx-auto">
                 <div class="card card-body bg-light mt-5">
                     <h2>Update User</h2>
