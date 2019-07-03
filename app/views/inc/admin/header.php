4dee5b64d64358ce20b65801b503e1f58a663d1a
Vukdrca <vukdrca@gmail.com>
2019-07-03 12:19:09 +0200

    new link  Sidebar - Brand  admini and parent

--- app/views/inc/admin/header.php
+++ app/views/inc/admin/header.php
@@ -46,7 +46,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
   <!-- Sidebar - Brand -->
-  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo URLROOT . '/users/teacher'; ?>">
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo URLROOT . '/users/admin'; ?>">
     <div class="sidebar-brand-icon rotate-n-15">
       <i class="fas fa-laugh-wink"></i>
     </div>
