4642b309476c9e5fa3168c86f556049a0e961e6f
ivanmitic91 <ivanmitic91@gmail.com>
2019-07-05 13:56:59 +0200

    added link for assign user

--- app/views/users/index.php
+++ app/views/users/index.php
@@ -15,82 +15,84 @@
                 <?php flash('user_deleted_msg') ?>
 
                 <a class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" href="<?php echo URLROOT; ?>/users/insert">Insert user</a>
-                
+                <a class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" href="<?php echo URLROOT; ?>/users/insert">Assign student</a>
+
                 <form action="<?php echo URLROOT; ?>/users/" method="post">
                     <div class="row select_role">
-                    <div class="col">
-                        <div class="form-group">
-                            <select name="user_role" id="user_role" class="form-control form-control-lg <?php echo (!empty($data['user_role_err'])) ? 'is-invalid' : '' ?>">
+                        <div class="col">
+                            <div class="form-group">
+                                <select name="user_role" id="user_role" class="form-control form-control-lg <?php echo (!empty($data['user_role_err'])) ? 'is-invalid' : '' ?>">
                                     <option value='' selected>.....Select a role.....</option>
                                     <?php
                                     $users = new Users();
                                     foreach ($users->GetUserRoles() as $id_user_role => $name) :
                                         ?>
                                         <option value='<?php echo $id_user_role; ?>'><?php print($name); ?></option>
-                                    <?php endforeach; ?>    
-                            </select>
-                            </div></div>
-                            <div class="col">            
-                                <input type="submit" class="btn btn-success" value="Show">
-                            </div> 
+                                    <?php endforeach; ?>
+                                </select>
+                            </div>
                         </div>
-                    </div>             
-                </form> 
+                        <div class="col">
+                            <input type="submit" class="btn btn-success" value="Show">
+                        </div>
+                    </div>
+            </div>
+            </form>
 
 
 
-                <div class="table-responsive">
-                    <table class="table table-striped">
+            <div class="table-responsive">
+                <table class="table table-striped">
 
-                        <thead>
+                    <thead>
+
+                        <tr>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>User Role</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+
+                    <?php
+                    $users = new Users();
+                    // if isset post user role get that value if not get ''
+                    foreach ($users->GetUsersByUserRole($_POST["user_role"] ?? '') as $user) : ?>
+
+                        <tbody>
 
                             <tr>
-                                <th>Username</th>
-                                <th>Email</th>
-                                <th>User Role</th>
-                                <th>Edit</th>
-                                <th>Delete</th>
+                                <td><?php echo ($user->username); ?></td>
+                                <td><?php print($user->email); ?></td>
+                                <td><?php printf($user->name); ?></td>
+                                <td>
+                                    <form action="<?php echo URLROOT . '/users/edit/' . $user->id_user; ?>" method="POST">
+
+                                        <a href="<?php echo URLROOT . '/users/edit/' . $user->id_user;; ?>" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">Edit</a>
+                                    </form>
+                                </td>
+                                <td>
+                                    <form action="<?php echo URLROOT . '/users/delete/'; ?>" method="POST">
+
+                                        <input type="submit" value="Delete" class="d-none d-sm-inline-block btn btn-sm btn-danger shadow-sm">
+                                    </form>
+                                </td>
                             </tr>
-                        </thead>
-                        
-                        <?php 
-                        $users = new Users();
-                        // if isset post user role get that value if not get ''
-                        foreach ($users->GetUsersByUserRole($_POST["user_role"] ?? '') as $user) : ?>
 
-                            <tbody>
+                        </tbody>
 
-                                <tr>
-                                    <td><?php echo($user->username); ?></td>
-                                    <td><?php print($user->email); ?></td>
-                                    <td><?php printf($user->name); ?></td>
-                                    <td>
-                                        <form action="<?php echo URLROOT . '/users/edit/'. $user->id_user; ?>" method="POST">
-                                            
-                                            <a href="<?php echo URLROOT . '/users/edit/' . $user->id_user;; ?>" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">Edit</a>
-                                        </form>
-                                    </td>
-                                    <td>
-                                        <form action="<?php echo URLROOT . '/users/delete/'; ?>" method="POST">
+                    <?php endforeach; ?>
 
-                                            <input type="submit" value="Delete" class="d-none d-sm-inline-block btn btn-sm btn-danger shadow-sm">
-                                        </form>
-                                    </td>
-                                </tr>
-
-                            </tbody>
-
-                        <?php endforeach; ?>
-
-                    </table>
-                </div>
-
+                </table>
             </div>
-        </div>
-        <!-- /.row -->
 
+        </div>
     </div>
-    <!-- /.container-fluid -->
+    <!-- /.row -->
+
+</div>
+<!-- /.container-fluid -->
 
 </div>
 <!-- /#page-wrapper -->
