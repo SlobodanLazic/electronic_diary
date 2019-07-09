007a509a6e05cd56fcd5d710e6547b47528b6166
Vaskep <panicvaskrsije@gmail.com>
2019-07-09 02:05:33 +0200

    stylize of teacher messages and edit code js

--- app/views/teacher/messages/index.php
+++ app/views/teacher/messages/index.php
@@ -1,53 +1,67 @@
 <?php require APPROOT . '/views/inc/teacher/header.php'; ?>
 
-<div id="page-wrapper">
-
-    <div class="container-fluid">
-         <div class="container">
-             <h2>Messager</h2>
-         </div>
-        
-        <div class="container border">
-            <div class="row">
-               <!-- Users -->
-               <div class="col-lg-4 p-1">
-                    
 
 
-               <h3>Parents</h3>
+<div class="container-mess">
+<h3 class=" text-center">Messaging</h3>
+<div class="messaging">
+      <div class="inbox_msg">
+        <div class="inbox_people">
+          <div class="headind_srch">
+            <div class="recent_heading">
+              <h4>Recent</h4>
+            </div>
+            <div class="srch_bar">
+              <div class="stylish-input-group">
+                <input type="text" class="search-bar"  placeholder="Search" >
+                
+                <span class="input-group-addon">
+                <button type="button"> <i class="fa fa-search" aria-hidden="true"></i> </button>
+                </span> </div>
+            </div>
+          </div>
+          <div class="inbox_chat">
+              <?php foreach($data['parents'] as $parent) { ?>
+            <div class="chat_list">
+              <div class="chat_people">
+                <div class="chat_img"> <img src="<?php echo URLROOT . "/images/parenticon.png" ?>" alt="sunil"> </div>
+                <div class="chat_ib">
+                  <?php echo "<h5 onclick($parent->id_user)> $parent->username "; ?><span class="chat_date">Date</span></h5>
+                  <p>Message</p>
+                </div>
+              </div>
+            </div> <?php } ?>
+          </div>
+        </div>
+        <div class="mesgs">
+        <input type="hidden" id="to_id" value='34'>
+          <div class="msg_history" id="messages">
+            <div class="incoming_msg">
+              <div class="incoming_msg_img"> <img src="<?php echo URLROOT . "/images/parenticon.png" ?>" alt="sunil"> </div>
+              <div class="received_msg message">
+                <div class="received_withd_msg">
+                  <p>Message mess asd messMessage mess asd messMessage mess asd messMessage mess asd mess</p>
+                  <span class="time_date">Date</span></div>
+              </div>
+            </div>
+            
+            <div class="outgoing_msg message">
+              <div class="sent_msg">
+                <p>Message mess asd messMessage mess asd messMessage mess asd messMessage mess asd messMessage mess asd mess</p>
+                <span class="time_date">Date</span> </div>
+            </div>
+          </div>
+          <div class="type_msg">
+            <div class="input_msg_write">
+              <input type="text" id="message" class="write_msg" placeholder="Type a message" />
+              <button class="msg_send_btn" type="button" onClick="sendMessage()"><i class="fas fa-paper-plane" aria-hidden="true" ></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </div></div>
 
-                <?php foreach($data['parents'] as $parent){
-                    
-                    echo "<p onClick($parent->id_user)>$parent->username</p>";
-
-                } ?>
-
-
-               </div>
-
-               <!-- Message -->
-
-               <div class="col-lg-8 p-1">
-                    <input type="hidden" id="to_id" value='34'>
-                    <div id="messages" class="container-fluid position-relative" style="Height:60vh">
-                    <div class="bg-success message">
-                          <span class="t_span">10:33</span>  <!-- Vreme nek bude tamno sive boje i float centar -->
-                        </div>   
-                       <div class="bg-success message">
-                          <span class="m_span">Ovo je poruka!!!</span>  <!-- poruke da idu levo i desno i razlicitih boja...   -->
-                        </div>
-                    </div>
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-lg-10">
-                                <textarea id="message" class="form-control" aria-label="With textarea"></textarea>
-                            </div>
-                            <div class="col-lg-2">
-                            <button type="button" class="btn btn-success m-2" onClick="sendMessage()">Send</button>
-                            </div>
-                        </div>
-                    </div>
-               </div>
             </div>
         </div>
     </div>
@@ -65,13 +79,26 @@
      {
         var message = document.getElementById('message').value;
 
-        var new_message = document.createElement("div"); 
+        var new_message = document.createElement("div"),
+            outgoing = document.createElement("div"),
+            par = document.createElement('p'),
+            date = document.createElement('span');
 
-        new_message.className = 'bg-success message m-1';
+        new_message.className = 'outgoing_msg message';
+        outgoing.className = 'sent_msg';
+        date.className = 'time_date';
+
+        new_message.appendChild(outgoing);
+        outgoing.appendChild(par);
+        outgoing.appendChild(date);
+
+        var datetext = document.createTextNode('Date'); 
+
+        date.appendChild(datetext);
 
         var text = document.createTextNode(message); 
 
-        new_message.appendChild(text); 
+        par.appendChild(text); 
 
        document.getElementById('message').value = ""; 
 
