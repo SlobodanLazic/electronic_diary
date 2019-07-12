43e198ee7f7a91112157f5bdb70b7f28f18e1249
ivanmitic91 <ivanmitic91@gmail.com>
2019-07-12 16:50:07 +0200

    Addeded dynamic creation for table in Teacher/Schedule

--- app/views/teacher/classes/schedule.php
+++ app/views/teacher/classes/schedule.php
@@ -21,40 +21,6 @@
 
                                     foreach ($data['schedules'] as $schedule) {
 
-
-                                        // if ($schedule->order_id == 1) {
-
-
-                                        //     $row1[] = $schedule;
-                                        // }
-
-                                        // if ($schedule->order_id == 2) {
-
-                                        //     $row2[] = $schedule;
-                                        // }
-
-                                        // if ($schedule->order_id == 3) {
-
-                                        //     $row3[] = $schedule;
-                                        // }
-                                        // if ($schedule->order_id == 4) {
-
-                                        //     $row4[] = $schedule;
-                                        // }
-                                        // if ($schedule->order_id == 5) {
-
-                                        //     $row5[] = $schedule;
-                                        // }
-                                        // if ($schedule->order_id == 6) {
-
-                                        //     $row6[] = $schedule;
-                                        // }
-                                        // if ($schedule->order_id == 7) {
-
-                                        //     $row7[] = $schedule;
-                                        // }
-
-
                                         switch ($schedule->order_id) {
 
                                             case 1:
@@ -104,134 +70,25 @@
 
                                                     if (isset($row1, $row2, $row3, $row4, $row5, $row6, $row7)) {
 
-                                                        echo "<td>1</td>";
+                                                        for ($i = 1; $i <= 7; $i++) {
 
-                                                        //TESTING NEW CODE , LET'S STAY FORE SOME TIME
+                                                            echo "<td>$i</td>";
 
-                                                        // foreach ($row1 as $r) {
 
-                                                        //     if (!empty($r->subject_name)) {
+                                                            // make dynamic variable
 
-                                                        //         echo "<td>$r->subject_name</td>";
-                                                        //     } else {
+                                                            $var = "row$i";
 
-                                                        //         echo "<td></td>";
-                                                        //     }
-                                                        // }
+                                                            $row = $$var;
 
-                                                        display_row($row1)
+                                                            // Inside Url Helper folder , displays table cells and link
 
-                                                        ?>
-                                                    </tr>
-                                                    <tr>
-                                                        <?php
-                                                        echo "<td>2</td>";
+                                                            display_row($row);
 
-                                                        //TESTING NEW CODE , LET'S STAY FORE SOME TIME IVAN    
-
-                                                        // foreach ($row2 as $v) {
-
-                                                        //     if (!empty($v->subject_name)) {
-
-                                                        //         echo "<td>$v->subject_name</td>";
-                                                        //     } else {
-
-                                                        //         echo "<td></td>";
-                                                        //     }
-                                                        // }
-
-                                                        display_row($row2)
-                                                        ?>
-                                                    </tr>
-                                                    <tr>
-                                                        <?php
-                                                        echo "<td>3</td>";
-                                                        // foreach ($row3 as $c) {
-
-                                                        //     if (!empty($c->subject_name)) {
-
-                                                        //         echo "<td>$c->subject_name</td>";
-                                                        //     } else {
-
-                                                        //         echo "<td></td>";
-                                                        //     }
-                                                        // }
-
-                                                        display_row($row3)
-                                                        ?>
-                                                    </tr>
-                                                    <tr>
-                                                        <?php
-                                                        echo "<td>4</td>";
-                                                        // foreach ($row4 as $f) {
-
-                                                        //     if (!empty($f->subject_name)) {
-
-                                                        //         echo "<td>$f->subject_name</td>";
-                                                        //     } else {
-
-                                                        //         echo "<td></td>";
-                                                        //     }
-                                                        // }
-
-                                                        display_row($row4)
-                                                        ?>
-                                                    </tr>
-                                                    <tr>
-                                                        <?php
-                                                        echo "<td>5</td>";
-                                                        // foreach ($row5 as $e) {
-
-                                                        //     if (!empty($e->subject_name)) {
-
-                                                        //         echo "<td>$e->subject_name</td>";
-                                                        //     } else {
-
-                                                        //         echo "<td></td>";
-                                                        //     }
-                                                        // }
-
-                                                        display_row($row5)
-
-                                                        ?>
-                                                    </tr>
-                                                    <tr>
-                                                        <?php
-                                                        echo "<td>6</td>";
-                                                        // foreach ($row6 as $g) {
-
-                                                        //     if (!empty($g->subject_name)) {
-
-                                                        //         echo "<td>$g->subject_name</td>";
-                                                        //     } else {
-
-                                                        //         echo "<td></td>";
-                                                        //     }
-                                                        // }
-                                                        display_row($row6)
-
-                                                        ?>
-                                                    </tr>
-                                                    <tr>
-                                                        <?php
-                                                        echo "<td>7</td>";
-                                                        // foreach ($row7 as $h) {
-
-                                                        //     if (!empty($h->subject_name)) {
-
-                                                        //         echo "<td>$h->subject_name</td>";
-                                                        //     } else {
-
-                                                        //         echo "<td></td>";
-                                                        //     }
-                                                        // }
-                                                        display_row($row7)
-
-                                                        ?>
-                                                    </tr>
-
-                                                <?php }
-                                            } ?>
+                                                            echo '</tr>';
+                                                        }
+                                                    }
+                                                } ?>
 
                                         </tbody>
                                     </table>
