13f9731e3b45a5cc06f8f525ac1fbd8b3b6548b2
Vukdrca <vukdrca@gmail.com>
2019-07-19 12:34:15 +0200

    Parent request on Teacher response page and removed request

--- public/js/teacher/response.js
+++ public/js/teacher/response.js
@@ -6,20 +6,31 @@ if (window.XMLHttpRequest) {
 }
 
 
+ 
 
-
-function aprove(id_meeting) {
-
-
+function aprove(id_meeting, x, r) {
+    
+    var btn = document.getElementsByClassName('btn'); 
+    var btn1 = "ab" + id_meeting;
+    var btn2 = "db" + id_meeting; 
 
     var xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function () {
 
-        if (xhttp.readyState == 4) {
-
-            alert('Status updated');
-
+        if (this.readyState == 4 && this.status == 200) {
+            var child = document.getElementById(id_meeting);
+            var parent = document.getElementById('tabela1'); 
+            if(x == 2) { 
+                document.getElementById(btn1).innerHTML = ""; 
+                document.getElementById(btn2).innerHTML = "<span class='text-success text-center'>Success</span>"
+                //id_row.childNodes[3].innerHTML = "Accept"
+            } else if(x == 1) {
+                var i = r.parentNode.parentNode.rowIndex;
+                document.getElementById("tabela1").deleteRow(i);
+            }
+           
+            
 
         }
 
@@ -29,7 +40,7 @@ function aprove(id_meeting) {
 
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-    var inputFromTeacher1 = "status=1&id_meeting=" + id_meeting;
+    var inputFromTeacher1 = "status="+status+"&id_meeting=" + id_meeting;
 
     xhttp.send(inputFromTeacher1);
 }
