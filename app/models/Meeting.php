6c4c41c8249a5f320ddad88feac04a74cf359619
ivanmitic91 <ivanmitic91@gmail.com>
2019-07-25 20:11:03 +0200

    fix conflict in meeting model

--- app/models/Meeting.php
+++ app/models/Meeting.php
@@ -14,16 +14,10 @@ class Meeting
 
         $this->db->query("INSERT INTO meetings(meetings.meetings, meetings.meetings_status, meetings.meeting_view, meetings.teacher) 
                           VALUES (:datatime, 0, 0, :teacher)");
-<<<<<<< HEAD
-    
-        $id_teacher = $_SESSION['id_user']; 
-        $this->db->bind(':teacher', $id_teacher); 
-=======
 
         $id_teacher = $_SESSION['id_user'];
 
         $this->db->bind(':teacher', $id_teacher);
->>>>>>> fbac48d9b367b91bc85b61b9a44e65800b669b25
         $this->db->bind(':datatime', $data);
 
 
@@ -64,32 +58,16 @@ class Meeting
         }
     }
 
-    public function getUserId($id)
-    {
-        $this->db->query("SELECT * FROM users WHERE id_user = :id_user"); 
-        $this->db->bind(':id_user', $id); 
-        $row = $this->db->single(); 
-        return $row; 
-    }
 
     public function selectTeacher($id_user)
     {
         $this->db->query("SELECT id_meetings, meetings, parent, meetings_status
                           FROM meetings 
-<<<<<<< HEAD
-                          WHERE teacher = :teacher AND meetings > now() 
-                          ORDER BY meetings.meetings ASC"); 
-        
-        $this->db->bind(':teacher', $id_user); 
-        $result = $this->db->resultSet(); 
-        return $result; 
-=======
                           WHERE teacher = :teacher AND meetings > now() ");
 
         $this->db->bind(':teacher', $id_user);
         $result = $this->db->resultSet();
         return $result;
->>>>>>> fbac48d9b367b91bc85b61b9a44e65800b669b25
     }
     /*
     public function selectParent($data) 
